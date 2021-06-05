8a8dfce1e26380c8b7dd0c626b422d6ea599c4e6
sharif7761 <sharif7761@gmail.com>
2021-06-06 01:26:06 +0600

    registration api

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Auth::routes();
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
