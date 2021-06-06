24f816f5f253cb92b1f781bcb1f348ec0bc936fb
sharif7761 <sharif7761@gmail.com>
2021-06-07 00:05:55 +0600

    sigup page design

--- frontend-react/src/components/Register.js
+++ frontend-react/src/components/Register.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Form, Button} from 'react-bootstrap'
 
 const Register = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [conPassword, setConPassword] = useState('')
+
+    function signup (e) {
+        e.preventDefault()
+
+    }
     return (
-        <div>
-            <h3>Register</h3>
+        <div className='col-6 offset-3 mt-5'>
+            <h2 className='text-center'>User Sign Up</h2>
+            <Form onSubmit={signup}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" onChange={(e) => setConPassword(e.target.value)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
