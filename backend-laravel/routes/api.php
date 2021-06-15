f665a82879056775adb4063f803689a6a8dca077
sharif7761 <sharif7761@gmail.com>
2021-06-15 23:41:38 +0600

    customer model, migration

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Auth::routes();
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [CustomerController::class, 'register']);
