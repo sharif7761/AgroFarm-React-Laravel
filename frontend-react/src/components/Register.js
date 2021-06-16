64e7fdd87f2fe20d51951eb8e8841906a50a345c
sharif7761 <sharif7761@gmail.com>
2021-06-17 00:54:07 +0600

    login page

--- frontend-react/src/components/Register.js
+++ frontend-react/src/components/Register.js
@@ -6,11 +6,20 @@ const Register = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [conPassword, setConPassword] = useState('')
 
-    function signup (e) {
+    async function signup(e) {
         e.preventDefault()
-
+        let item = {name, email, password}
+        let result = await fetch('http://127.0.0.1:8000/api/register',{
+            method: 'POST',
+            headers: {
+                "Content-Type" : "application/json",
+                "Accept" : "application/json"
+            },
+            body: JSON.stringify(item)
+        })
+        result = await result.json();
+        console.log(result)
     }
     return (
         <div className='col-6 offset-3 mt-5'>
@@ -18,22 +27,18 @@ const Register = () => {
             <Form onSubmit={signup}>
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+                    <Form.Control type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm Password" onChange={(e) => setConPassword(e.target.value)} />
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
