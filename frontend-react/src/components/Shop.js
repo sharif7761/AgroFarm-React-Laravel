87b9cc317819611ef5a0b6783fd98519cc5c11bd
sharif7761 <sharif7761@gmail.com>
2021-06-19 23:45:57 +0600

    shop page design

--- frontend-react/src/components/Shop.js
+++ frontend-react/src/components/Shop.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import Veg from '../assets/images/veg.jpg'
+import Friuts from '../assets/images/fruits.jpg'
+import Crops from '../assets/images/crops.jpg'
 
 const Shop = () => {
     return (
-        <div>
-            <h3>Shop Home</h3>
-        </div>
+        <section className='container'>
+            <div className='row my-5'>
+                <div className='col-12 col-md-6'>
+                    <div className="card" >
+                        <img className="card-img-top" src={ Crops } height="250" alt="Card image cap" />
+                        <div className="card-body m-auto">
+                            <h5 className="card-title text-center">Crops</h5>
+                            <a href="#" className="btn btn-primary">View All</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6'>
+                    <div className="card" >
+                        <img className="card-img-top" src={ Friuts } height="250" alt="Card image cap" />
+                        <div className="card-body m-auto">
+                            <h5 className="card-title text-center">Fruits</h5>
+                            <a href="#" className="btn btn-primary">View All</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 offset-md-3 mt-5'>
+                    <div className="card" >
+                        <img className="card-img-top" src={ Veg } height="250" alt="Card image cap" />
+                        <div className="card-body m-auto">
+                            <h5 className="card-title text-center">Vegetables</h5>
+                            <a href="#" className="btn btn-primary">View All</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
